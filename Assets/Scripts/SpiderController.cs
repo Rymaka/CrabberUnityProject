d5222272d07aaada8d1bec9488ec7668e15e740e
Bulat Igebaev <pingvin13lol@gmail.com>
2025-01-18 18:28:11 +0300

    Added first iteration of spider

---
+++ Assets/Scripts/SpiderController.cs
@@ -0,0 +1,105 @@
+﻿using UnityEngine;
+
+public class SpiderController : MonoBehaviour
+{
+    [SerializeField] private float _speed = 3f;
+    [SerializeField] private float smoothness = 5f;
+    [SerializeField] private int raysNb = 8;
+    [SerializeField] private float raysEccentricity = 0.2f;
+    [SerializeField] private float outerRaysOffset = 2f;
+    [SerializeField] private float innerRaysOffset = 25f;
+
+    private Vector3 velocity;
+    private Vector3 lastVelocity;
+    private Vector3 lastPosition;
+    private Vector3 forward;
+    private Vector3 upward;
+    private Quaternion lastRot;
+    private Vector3[] pn;
+
+    private static Vector3[] GetClosestPoint(Vector3 point, Vector3 forward, Vector3 up, float halfRange,
+        float eccentricity, float offset1, float offset2, int rayAmount)
+    {
+        var result = new Vector3[2] { point, up };
+        var right = Vector3.Cross(up, forward);
+        var normalAmount = 1f;
+        var positionAmount = 1f;
+
+        var dirs = new Vector3[rayAmount];
+        var angularStep = 2f * Mathf.PI / rayAmount;
+        var currentAngle = angularStep / 2f;
+        for (var i = 0; i < rayAmount; ++i)
+        {
+            dirs[i] = -up + (right * Mathf.Cos(currentAngle) + forward * Mathf.Sin(currentAngle)) * eccentricity;
+            currentAngle += angularStep;
+        }
+
+        foreach (var dir in dirs)
+        {
+            RaycastHit hit;
+            var largener = Vector3.ProjectOnPlane(dir, up);
+            var ray = new Ray(point - (dir + largener) * halfRange + largener.normalized * offset1 / 100f, dir);
+            if (Physics.SphereCast(ray, 0.01f, out hit, 2f * halfRange))
+            {
+                result[0] += hit.point;
+                result[1] += hit.normal;
+                normalAmount += 1;
+                positionAmount += 1;
+            }
+
+            ray = new Ray(point - (dir + largener) * halfRange + largener.normalized * offset2 / 100f, dir);
+            if (Physics.SphereCast(ray, 0.01f, out hit, 2f * halfRange))
+            {
+                result[0] += hit.point;
+                result[1] += hit.normal;
+                normalAmount += 1;
+                positionAmount += 1;
+            }
+        }
+
+        result[0] /= positionAmount;
+        result[1] /= normalAmount;
+        return result;
+    }
+
+    private void Start()
+    {
+        velocity = new Vector3();
+        forward = transform.forward;
+        upward = transform.up;
+        lastRot = transform.rotation;
+    }
+
+    private void FixedUpdate()
+    {
+        velocity = (smoothness * velocity + (transform.position - lastPosition)) / (1f + smoothness);
+        if (velocity.magnitude < 0.00025f)
+            velocity = lastVelocity;
+        lastPosition = transform.position;
+        lastVelocity = velocity;
+
+        //TODO: Remake this controller to mobile gamepad style + PC (WASD) with unity new input system
+        var valueY = Input.GetAxis("Vertical");
+        if (valueY != 0)
+            transform.position += transform.forward * valueY * _speed * Time.fixedDeltaTime;
+        var valueX = Input.GetAxis("Horizontal");
+        if (valueX != 0)
+            transform.position += Vector3.Cross(transform.up, transform.forward) * valueX * _speed *
+                                  Time.fixedDeltaTime;
+
+        if (valueX != 0 || valueY != 0)
+        {
+            pn = GetClosestPoint(transform.position, transform.forward, transform.up, 0.5f, 0.1f, 30, -30, 4);
+            upward = pn[1];
+            var position = GetClosestPoint(transform.position, transform.forward, transform.up, 0.5f, raysEccentricity,
+                innerRaysOffset, outerRaysOffset, raysNb);
+            transform.position = Vector3.Lerp(lastPosition, position[0], 1f / (1f + smoothness));
+
+            forward = velocity.normalized;
+            var quaternion = Quaternion.LookRotation(forward, upward);
+            transform.rotation = Quaternion.Lerp(lastRot, quaternion, 1f / (1f + smoothness));
+        }
+
+        lastRot = transform.rotation;
+    }
+}
---
+++ Assets/Scripts/SpiderProceduralAnimation.cs
@@ -0,0 +1,163 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class SpiderProceduralAnimation : MonoBehaviour
+{
+    [SerializeField] private Transform[] legTargets;
+    [SerializeField] private float stepSize = 0.15f;
+    [SerializeField] private int smoothness = 8;
+    [SerializeField] private float stepHeight = 0.15f;
+    [SerializeField] private float sphereCastRadius = 0.125f;
+    [SerializeField] private bool bodyOrientation = false;
+
+    [SerializeField] private float raycastRange = 1.5f;
+    private Vector3[] defaultLegPositions;
+    private Vector3[] lastLegPositions;
+    private Vector3 lastBodyUp;
+    private bool[] legMoving;
+    private int nbLegs;
+
+    private Vector3 velocity;
+    private Vector3 lastVelocity;
+    private Vector3 lastBodyPos;
+
+    private float velocityMultiplier = 15f;
+
+    private Vector3[] MatchToSurfaceFromAbove(Vector3 point, float halfRange, Vector3 up)
+    {
+        var res = new Vector3[2];
+        res[1] = Vector3.zero;
+        RaycastHit hit;
+        var ray = new Ray(point + halfRange * up / 2f, -up);
+
+        if (Physics.SphereCast(ray, sphereCastRadius, out hit, 2f * halfRange))
+        {
+            res[0] = hit.point;
+            res[1] = hit.normal;
+        }
+        else
+        {
+            res[0] = point;
+        }
+
+        return res;
+    }
+
+    private void Start()
+    {
+        lastBodyUp = transform.up;
+
+        nbLegs = legTargets.Length;
+        defaultLegPositions = new Vector3[nbLegs];
+        lastLegPositions = new Vector3[nbLegs];
+        legMoving = new bool[nbLegs];
+        for (int i = 0; i < nbLegs; ++i)
+        {
+            defaultLegPositions[i] = legTargets[i].localPosition;
+            lastLegPositions[i] = legTargets[i].position;
+            legMoving[i] = false;
+        }
+
+        lastBodyPos = transform.position;
+    }
+
+    private IEnumerator PerformStep(int index, Vector3 targetPoint)
+    {
+        var startPos = lastLegPositions[index];
+        for (int i = 1; i <= smoothness; ++i)
+        {
+            legTargets[index].position = Vector3.Lerp(startPos, targetPoint, i / (smoothness + 1f));
+            legTargets[index].position +=
+                transform.up * Mathf.Sin(i / (smoothness + 1f) * Mathf.PI) * stepHeight;
+            yield return new WaitForFixedUpdate();
+        }
+
+        legTargets[index].position = targetPoint;
+        lastLegPositions[index] = legTargets[index].position;
+        legMoving[0] = false;
+    }
+
+
+    private void FixedUpdate()
+    {
+        velocity = transform.position - lastBodyPos;
+        velocity = (velocity + smoothness * lastVelocity) / (smoothness + 1f);
+
+        if (velocity.magnitude < 0.000025f)
+        {
+            velocity = lastVelocity;
+        }
+        else
+        {
+            lastVelocity = velocity;
+        }
+        
+        Vector3[] desiredPositions = new Vector3[nbLegs];
+        var indexToMove = -1;
+        var maxDistance = stepSize;
+        for (int i = 0; i < nbLegs; ++i)
+        {
+            desiredPositions[i] = transform.TransformPoint(defaultLegPositions[i]);
+
+            var distance = Vector3
+                .ProjectOnPlane(desiredPositions[i] + velocity * velocityMultiplier - lastLegPositions[i], transform.up)
+                .magnitude;
+            if (distance > maxDistance)
+            {
+                maxDistance = distance;
+                indexToMove = i;
+            }
+        }
+
+        for (int i = 0; i < nbLegs; ++i)
+            if (i != indexToMove)
+                legTargets[i].position = lastLegPositions[i];
+
+        if (indexToMove != -1 && !legMoving[0])
+        {
+            var targetPoint = desiredPositions[indexToMove] +
+                                  Mathf.Clamp(velocity.magnitude * velocityMultiplier, 0.0f, 1.5f) *
+                                  (desiredPositions[indexToMove] - legTargets[indexToMove].position) +
+                                  velocity * velocityMultiplier;
+
+            Vector3[] positionAndNormalFwd = MatchToSurfaceFromAbove(targetPoint + velocity * velocityMultiplier,
+                raycastRange, (transform.parent.up - velocity * 100).normalized);
+            Vector3[] positionAndNormalBwd = MatchToSurfaceFromAbove(targetPoint + velocity * velocityMultiplier,
+                raycastRange * (1f + velocity.magnitude), (transform.parent.up + velocity * 75).normalized);
+
+            legMoving[0] = true;
+
+            if (positionAndNormalFwd[1] == Vector3.zero)
+            {
+                StartCoroutine(PerformStep(indexToMove, positionAndNormalBwd[0]));
+            }
+            else
+            {
+                StartCoroutine(PerformStep(indexToMove, positionAndNormalFwd[0]));
+            }
+        }
+
+        lastBodyPos = transform.position;
+        if (nbLegs > 3 && bodyOrientation)
+        {
+            var v1 = legTargets[0].position - legTargets[1].position;
+            var v2 = legTargets[2].position - legTargets[3].position;
+            var normal = Vector3.Cross(v1, v2).normalized;
+            var up = Vector3.Lerp(lastBodyUp, normal, 1f / (float)(smoothness + 1));
+            transform.up = up;
+            transform.rotation = Quaternion.LookRotation(transform.parent.forward, up);
+            lastBodyUp = transform.up;
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        for (int i = 0; i < nbLegs; ++i)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(legTargets[i].position, 0.05f);
+            Gizmos.color = Color.green;
+            Gizmos.DrawWireSphere(transform.TransformPoint(defaultLegPositions[i]), stepSize);
+        }
+    }
+}
